11b0ece7d8105d5bbbf75a6d53f25797e58365ce
GodAresIT <phamtuan111203@gmail.com>
2023-12-02 16:24:40 +0700

    changed source code

--- NewsWebstieJava/src/main/java/com/example/NewsWebstieJava/Repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.NewsWebstieJava.Repository;
-
-import com.example.NewsWebstieJava.Models.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
